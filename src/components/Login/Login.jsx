b2aa1445f6d77c882ce9032ed0846c03998d9f09
Nicolás Arévalo <inag.458@gmail.com>
2022-02-14 20:07:17 -0500

    Add Login
    First component, Login, also installed some dependencies as Sass and the compiler... Now login works, and we can navigate.

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { useNavigate }  from 'react-router-dom';
+
+import './Login.scss';
+
+
+
+export const Login = () => {
+
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Hola, ${username}, bienvenido.`);
+        navigate('/');
+    }
+
+    return (
+        <div className="login">
+            <div className='login-wrapper'>
+                <h1>Movies</h1>
+                <form onClick={handleSubmit} value='Your username' className='login_form'>
+                    <input
+                        type="text" name='username'
+                        onChange={(e) => setUsername(e.target.value)} />
+                    <input
+                        type="password"
+                        onChange={({ target }) => setPassword(target.value)} />
+                    <button>Get in</button>
+                </form>
+
+            </div>
+        </div>
+
+    )
+}
